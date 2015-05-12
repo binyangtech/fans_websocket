f34e8db09f60e541a06667c044e4d42f4fe6ec99
David Ruan <ruanwz@gmail.com>
2015-05-12 10:05:21 +0800

    change ws address

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -3,7 +3,8 @@ import {Socket} from "phoenix"
 let chatInput         = $("#chat-input")
 let messagesContainer = $("#messages")
 
-let socket = new Socket("ws://ruanwz.me:4000/ws")
+#let socket = new Socket("ws://ruanwz.me:4000/ws")
+let socket = new Socket("/ws")
 
 socket.connect()
 socket.join("rooms:lobby", {}).receive("ok", chan => {
